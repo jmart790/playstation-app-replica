429a415fea2ce9ba3d751a7c1d35f5a7456912bd
Josh <jmart790@fiu.edu>
2021-01-14 18:33:29 -0500

    adding vue router to project

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div id="app">
-
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-
-
-export default {
-  name: 'App',
-}
-</script>
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-}
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background-color: #101116;
-  color: #F1F1F1;
-  height: 100vh;
-  width: 100vw;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>

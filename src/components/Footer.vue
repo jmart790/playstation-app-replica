d702c1019486c854591e8a716f8f831932f253ea
Josh <jmart790@fiu.edu>
2021-01-14 20:57:28 -0500

    created empty footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer">
+    <h1>footer</h1>
+  </footer>
+</template>
+
+<script>
+export default {
+name: "Footer"
+}
+</script>
+
+<style lang="scss">
+.footer {
+  width: 100%;
+  height: 50px;
+  border: 2px solid gray;
+}
+</style>

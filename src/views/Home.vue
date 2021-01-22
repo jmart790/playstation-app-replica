39935836d6b371edc68b78f8b055955c87c75d45
Josh <jmart790@fiu.edu>
2021-01-21 20:34:47 -0500

    importing components into home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="home">
-    Home aka Play
-  </div>
+  <main class="home">
+    <HomeHeader class="home__header"/>
+    <RecentlyPlayed class="home__recently-played"/>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
+import HomeHeader from '@/components/home/HomeHeader'
+import RecentlyPlayed from '@/components/home/RecentlyPlayed.vue'
+
 
 export default {
+  components: { HomeHeader, RecentlyPlayed },
   name: 'Home',
 }
 </script>
+
+<style lang="scss">
+.home {
+  &__header {
+    margin-bottom: gap(5);
+  }
+}
+</style>

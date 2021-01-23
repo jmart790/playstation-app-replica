32e53f8dc2515ddf1628aa3fb447ecf7cfe8ef85
Josh <jmart790@fiu.edu>
2021-01-22 21:29:11 -0500

    added new components to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <main class="home">
     <HomeHeader class="home__header"/>
+    <FriendsPlaying class="home__friends-playing"/>
     <RecentlyPlayed class="home__recently-played"/>
   </main>
 </template>
@@ -8,10 +9,11 @@
 <script>
 import HomeHeader from '@/components/home/HomeHeader'
 import RecentlyPlayed from '@/components/home/RecentlyPlayed.vue'
+import FriendsPlaying from '@/components/home/FriendsPlaying.vue'
 
 
 export default {
-  components: { HomeHeader, RecentlyPlayed },
+  components: { HomeHeader, RecentlyPlayed, FriendsPlaying },
   name: 'Home',
 }
 </script>
@@ -19,7 +21,13 @@ export default {
 <style lang="scss">
 .home {
   &__header {
-    margin-bottom: gap(5);
+    margin-bottom: gap(6);
+  }
+  &__friends-playing {
+    margin-bottom: gap(8);
+  }
+  &__recently-played {
+    margin-bottom: gap(10);
   }
 }
 </style>
